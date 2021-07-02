763141e20dc0b2159798184b5e42e1195ea30af9
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-02 15:05:33 +0900

    標準で用意されてるレイアウトを使ってみた

---
+++ TableViewBeginner/TableView2ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TableView2ViewController.swift
+//  TableViewBeginner
+//
+//  Created by IwasakIYuta on 2021/07/02.
+//
+
+import UIKit
+
+class TableView2ViewController : UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+  
+
+}
+extension TableView2ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20 //rowの数（セルの数）
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //標準で設定されてるレイアウトを使ってみた
+        var cellid = ""
+        switch indexPath.row {
+        case 0:
+            cellid = "cell1"
+        case 1:
+            cellid = "cell2"
+        case 2:
+            cellid = "cell3"
+        case 3:
+            cellid = "cell4"
+        default://switchは全部網羅しないとエラーが出るのでindexPath.rowが４以上の時はカスタムセルが出るように設定
+            cellid = "cell5"
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid) ?? UITableViewCell()
+        cell.textLabel?.text = "text1"
+        cell.detailTextLabel?.text = "text2"
+        return cell
+    }
+    
+    
+    
+}
--- TableViewBeginner/ViewController.swift
+++ TableViewBeginner/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var cellSample = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
+}
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellSample.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //セルをある条件で切り替える↓
+     var identity = ""
+        //配列の順番で偶数と奇数で切り替える
+        if indexPath.row % 2 == 0 {
+            identity = "Cell1"
+        } else {
+            identity = "Cell2"
+        }
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identity) ?? UITableViewCell(style: .default, reuseIdentifier: identity)
+        cell.textLabel?.text = cellSample[indexPath.row]
+        return cell
+    }
+    @IBAction func exit(segue: UIStoryboardSegue){}
+    
+    
 }
 
